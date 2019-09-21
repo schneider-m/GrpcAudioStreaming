1293098ad1ee8cdd9bf02e4cc6e329df9256277d
Martin Schneider <schneider-m@users.noreply.github.com>
2019-09-21 15:48:31 +0200

    added grpc client

---
+++ GrpcAudioStreaming.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GrpcAudioStreaming.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // When calling insecure gRPC services this switch must be set before creating the GrpcChannel/HttpClient.
+            // https://docs.microsoft.com/en-us/aspnet/core/grpc/troubleshoot?view=aspnetcore-3.0#call-insecure-grpc-services-with-net-core-client
+            //AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            // The port number(5001) must match the port of the gRPC server.
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
